ebf6143fbb5235ec40a46dd817ad4a196ab2949e
Andrew Melad <andrewmelad108@gmail.com>
2024-08-27 16:58:58 +0300

    change files

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "VTablePackage",
-      fileName: "v-table-package",
+      name: "TableVue",
+      fileName: "table-vue",
     },
     rollupOptions: {
       external: ["vue"],
